33e0d55cb5c3f1e72c81f825d6349b9eeab79853
梁超 <875883972@qq.com>
2016-09-07 11:00:47 +0800

    index

---
+++ javascripts/index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Administrator on 2016/9/7.
+ */
+$(function () {
+    $('#fullpage').fullpage({
+        'verticalCentered': false, /*内容是否垂直居中*/
+        'css3': true, /*是否使用 CSS3 transforms 滚动*/
+        anchors: ['page1', 'page2', 'page3', 'page4'], /*定义锚链接*/
+        'navigation': true, /*是否显示项目导航*/
+        'navigationPosition': 'right', /*项目导航的位置，可选 left 或 right*/
+        'navigationTooltips': ['HOME', 'about', 'help', 'connect'], /*项目导航的 tip*/
+        loopBottom: false, /*滚动到最底部后是否滚回顶部*/
+        loopTop: false   /*滚动到最顶部后是否滚底部*/
+    })
+    $(".navMenu").click(function () {
+        $(".navMenu").removeClass("active");
+        $(this).addClass("active");
+    });
+    setInterval(function () {
+        var sUrl = window.location.hash;
+        sUrl = sUrl.split("#");
+//   console.log(sUrl[1]);
+    }, 500)
+
+    /*获取第一屏下面的文字位置*/
+    var windowH = $(window).height();
+    $('.section_1_wenzi').css({
+        'margin-top' :windowH *.3+'px'
+    })
+})
